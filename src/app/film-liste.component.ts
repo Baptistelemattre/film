4040151b77276dff7b07722aac6c236ef49d8bf0
Baptiste Lemattre <baptiste_lemattre@ens.univ-artois.fr>
2022-03-02 10:44:53 +0100

    question 2.3

---
+++ src/app/film-liste.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-film-liste',
+  template: `
+    <p>
+      film-liste works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FilmListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
