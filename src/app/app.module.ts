8f3d60b8baeb92bb7abd28b2dbfccad0e1778f0d
Baptiste Lemattre <baptiste_lemattre@ens.univ-artois.fr>
2022-03-02 10:44:10 +0100

    question 2.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatSliderModule} from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilmListeComponent } from './film-liste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FilmListeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/film.ts
@@ -0,0 +1,34 @@
+export interface Film{
+  title: string;
+  year: number;
+  director: string;
+  actors:string[]
+}
+export const Films:Film[] = [
+  {
+    title: 'Top Gun',
+    year:1986,
+    director:'Tony Scott',
+    actors : ['Tom Cruise', 'Kelly McGillis', 'Val Kilmer', 'Anthony Edwards', 'Tom Skerritt']
+  },
+  {
+    title: 'The Dark Knight Rises',
+    year: 2012,
+    director: 'Christoper Nolan',
+    actors: ['Christian Bale', 'Michael Caine', 'Gary Oldman', 'Marion Cotillard', 'Joseph Gordon-Levitt']
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    director: 'Christoper Nolan',
+    actors: ['Leonardo DiCaprio', 'Ellen Page', 'Marion Cotillard', 'Joseph Gordon-Levitt']
+  },
+  {
+    title: 'Child 44',
+    year: 2015,
+    director: 'Daniel Espinosa',
+    actors: ['Tom hardy', 'Noomi Rapace', 'Gary Oldman','Vincent Cassel']
+  }
+];
+
+
